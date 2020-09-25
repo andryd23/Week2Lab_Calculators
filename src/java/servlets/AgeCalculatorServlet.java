93f0d5466db59af229985d869b3e6c13c472d37c
832465 <832465@SAIT225779IT.ACDM.DS.SAIT.CA>
2020-09-25 14:09:54 -0600

    New project , creating calculator Managed to get the first part completed.

---
+++ src/java/servlets/AgeCalculatorServlet.java
@@ -0,0 +1,59 @@
+
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 832465
+ */
+@WebServlet(name = "AgeCalculatorServlet", urlPatterns = {"/AgeCalculatorServlet"})
+public class AgeCalculatorServlet extends HttpServlet {
+    
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      int userAge;
+      int nextAge;
+      userAge = Integer.parseInt(request.getParameter("user_age"));
+      request.setAttribute("user_age", userAge);
+        
+        if(userAge < 0 || userAge == 0) {
+            request.setAttribute("message", "You must give your current age");
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+            return;
+        }
+        else if(userAge != (int)userAge) {
+            request.setAttribute("message", "You must enter a number");
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+            return;
+        }
+        
+        else {
+            nextAge = userAge + 1;
+            request.setAttribute("message", "Your age next birthday will be " + nextAge);
+            getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+        }
+       
+      
+  }
+    
+    
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException  {
+      
+      getServletContext().getRequestDispatcher("/WEB-INF/agecalculator.jsp").forward(request, response);
+      
+      
+  }
+  
+  
+    
+    
+}
+
